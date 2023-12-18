ebdd4e70e73231e994e5238cdcc54405db3f0f17
MCeciliaLuna <lunama.cecilia@live.com>
2023-12-18 20:50:25 -0300

    initial template

---
+++ README.md
@@ -0,0 +1,6 @@
+# 02-RC-Curso-react
+
+Ejercicio TASK en reac donde aplicamos lo aprendido en la clase 02 que incluye:
+
+Currently, two official plugins are available:
+
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import { TaskHead } from './TaskHead';
+import './styles/index.css';
+
+
+
+
+const App = () => {
+ 
+  
+  return (
+    <TaskHead />
+  );
+}
+
+export default App
